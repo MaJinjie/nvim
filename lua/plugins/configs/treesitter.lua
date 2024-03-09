e755c195f225231089d5954993d0a5f836379895
MaJinjie <2734883936@qq.com>
2024-03-09 10:29:23 +0800

    init nvchad

---
+++ lua/plugins/configs/treesitter.lua
@@ -0,0 +1,12 @@
+local options = {
+  ensure_installed = { "lua", "vim", "vimdoc" },
+  parser_install_dir = nil,
+  highlight = {
+    enable = true,
+    use_languagetree = true,
+  },
+
+  indent = { enable = true },
+}
+
+return options
