131c61ef397a372febfeffddcef47b7f692cb3ba
majinjie <2734883936@qq.com>
2024-09-03 10:59:00 +0800

    feat(mini-pair): map MiniPairs.bs() to <C-h> <C-w> <C-u>

--- lua/plugins/easy-peasy.lua
+++ lua/plugins/easy-peasy.lua
@@ -65,6 +65,15 @@ return {
   },
   {
     "echasnovski/mini.pairs",
+    init = function()
+      local map_bs = function(lhs, rhs)
+        vim.keymap.set({ "i", "c" }, lhs, rhs, { expr = true, replace_keycodes = false })
+      end
+
+      map_bs("<C-h>", "v:lua.MiniPairs.bs()")
+      map_bs("<C-w>", 'v:lua.MiniPairs.bs("\23")')
+      map_bs("<C-u>", 'v:lua.MiniPairs.bs("\21")')
+    end,
     opts = { mappings = { ["'"] = false } },
   },
 }
