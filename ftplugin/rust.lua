f947542d560c07be1e97ed40d4cab3956729169e
majinjie <2734883936@qq.com>
2024-10-04 00:00:56 +0800

    fix(keymaps):修改一些常用的和rust快捷键

--- ftplugin/rust.lua
+++ ftplugin/rust.lua
@@ -1,19 +1,11 @@
-local map = vim.keymap.set
+local set = vim.keymap.set
 
-map("n", "<leader><localleader>r", "<cmd>RustLsp run<cr>", { buffer = true })
-map("n", "<leader><localleader>R", "<cmd>RustLsp runables<cr>", { buffer = true })
-map("n", "<leader><localleader>lr", "<cmd>RustLsp! run<cr>", { buffer = true })
-map("n", "<leader><localleader>lR", "<cmd>RustLsp! runables<cr>", { buffer = true })
+set("n", "<localleader>r", "<cmd>RustLsp run<cr>", { buffer = true })
+set("n", "<localleader>R", "<cmd>RustLsp runables<cr>", { buffer = true })
+set("n", "<localleader>t", "<cmd>RustLsp testables<cr>", { buffer = true })
+set("n", "<localleader>lr", "<cmd>RustLsp! run<cr>", { buffer = true })
+set("n", "<localleader>lR", "<cmd>RustLsp! runables<cr>", { buffer = true })
+set("n", "<localleader>lt", "<cmd>RustLsp! testables<cr>", { buffer = true })
 
-map("n", "<leader><localleader>e", "<cmd>RustLsp explainError current<cr>", { buffer = true })
-map("n", "<leader><localleader>E", "<cmd>RustLsp expandMacro<cr>", { buffer = true })
-
-map("n", "<leader><localleader>o", "<cmd>RustLsp openCargo<cr>", { buffer = true })
-map("n", "<leader><localleader>p", "<cmd>RustLsp parentModule<cr>", { buffer = true })
-
-map("n", "<leader><localleader>t", "<cmd>RustLsp testables<cr>", { buffer = true })
-map("n", "<leader><localleader>lt", "<cmd>RustLsp! testables<cr>", { buffer = true })
-
-map("n", "<leader><localleader>ar", "<cmd>RustAnalyzer restart<cr>", { buffer = true })
-map("n", "<leader><localleader>aR", "<cmd>RustAnalyzer reloadSettings<cr>", { buffer = true })
-map("n", "<leader><localleader>as", "<cmd>RustAnalyzer stop<cr>", { buffer = true })
+set("n", "<localleader>oc", "<cmd>RustLsp openCargo<cr>", { buffer = true })
+set("n", "<localleader>op", "<cmd>RustLsp parentModule<cr>", { buffer = true })
--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -4,10 +4,8 @@
 local map = vim.keymap.set
 
 map("i", "<C-e>", "<End>", { desc = "Jump to Current line end" })
-map("n", "<leader>ww", "<C-W>p", { desc = "Switch to Other Window", remap = true })
 map("i", "<C-CR>", "<Esc>o", { desc = "Jump to Next line" })
-
--- floating terminal
---  stylua: ignore
-local lazyterm = function() LazyVim.terminal(nil, { cwd = LazyVim.root() }) end
-map("i", "<c-/>", lazyterm, { desc = "Terminal (Root Dir)" })
+map({ "i", "x", "n", "s" }, "<C-s>", function()
+   vim.cmd("w")
+   LazyVim.format.format({ force = true })
+end, { desc = "Save File And Format File" })
