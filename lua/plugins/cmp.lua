7be098d01d6a1132069fda126763ccd9a7ccde76
majinjie <2734883936@qq.com>
2024-10-04 00:04:01 +0800

    fix(cmp): 为一些特定的文件类型取消snippets补全源
    因为有些鸡肋，和其他补全项混淆

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -7,31 +7,40 @@
 --   auto_brackets = { "python" }
 -- }
 -- ```
+local function get_specified_source_id(sources, source_name)
+   for id, source in pairs(sources) do
+      if source.name == source_name then
+         return id
+      end
+   end
+end
 return {
    {
       "hrsh7th/nvim-cmp",
       opts = function(_, opts)
          local cmp = require("cmp")
+         local default = require("cmp.config.default")()
+         local compare = require("cmp.config.compare")
+
          local keymappings = {}
          -- local auto_select = true
 
-         keymappings["<C-e>"] = function(fallback)
-            if LazyVim.cmp.visible() then
-               cmp.abort()
-            else
-               fallback()
-            end
-         end
+         -- keymappings["<C-e>"] = function(fallback)
+         --    if LazyVim.cmp.visible() then
+         --       cmp.abort()
+         --    else
+         --       fallback()
+         --    end
+         -- end
          -- keymappings["<CR>"] = LazyVim.cmp.confirm({ select = auto_select })
 
-         keymappings = vim.tbl_map(function(value)
-            return cmp.mapping(value, { "i" })
-         end, keymappings)
+         -- keymappings = vim.tbl_map(function(value)
+         --    return cmp.mapping(value, { "i" })
+         -- end, keymappings)
 
          -- opts.completion = { completeopt = "menu,menuone,noinsert" .. (auto_select and "" or ",noselect") }
          -- opts.preselect = auto_select and cmp.PreselectMode.Item or cmp.PreselectMode.None
-         opts.mapping = vim.tbl_extend("force", opts.mapping, keymappings)
-         opts.sorting = nil
+         -- opts.mapping = vim.tbl_extend("force", opts.mapping, keymappings)
       end,
    },
    {
@@ -42,25 +51,26 @@ return {
          local cmp = require("cmp")
          local keymappings = {}
 
-         keymappings["<C-e>"] = function(fallback)
-            if LazyVim.cmp.visible() then
-               cmp.abort()
-            else
-               fallback()
-            end
-         end
+         -- keymappings["<C-e>"] = function(fallback)
+         --    if LazyVim.cmp.visible() then
+         --       cmp.abort()
+         --    else
+         --       fallback()
+         --    end
+         -- end
+         --
+         -- keymappings = vim.tbl_map(function(value)
+         --    return cmp.mapping(value, { "c" })
+         -- end, keymappings)
+         -- keymappings = cmp.mapping.preset.cmdline(keymappings)
 
-         keymappings = vim.tbl_map(function(value)
-            return cmp.mapping(value, { "c" })
-         end, keymappings)
-
-         keymappings = cmp.mapping.preset.cmdline(keymappings)
+         keymappings = cmp.mapping.preset.cmdline()
 
          -- `/` cmdline setup.
          cmp.setup.cmdline({ "/", "?" }, {
             completion = { completeopt = "menu,menuone,noinsert,noselect" },
             preselect = cmp.PreselectMode.None,
-            mapping = keymappings,
+            mapping = cmp.mapping.preset.cmdline(),
             sources = {
                { name = "buffer" },
             },
@@ -69,7 +79,7 @@ return {
          cmp.setup.cmdline(":", {
             completion = { completeopt = "menu,menuone,noinsert,noselect" },
             preselect = cmp.PreselectMode.None,
-            mapping = keymappings,
+            mapping = cmp.mapping.preset.cmdline(),
             sources = cmp.config.sources({
                { name = "path" },
             }, {
@@ -80,6 +90,31 @@ return {
                   },
                },
             }),
+         })
+      end,
+   },
+   -- disable snippets
+   {
+      "garymjr/nvim-snippets",
+      init = function()
+         -- 排除某些文件类型的snippets
+         vim.api.nvim_create_autocmd("FileType", {
+            pattern = { "sql" },
+            callback = function(ev)
+               local cmp = require("cmp")
+
+               -- global sources
+               ---@param source cmp.SourceConfig
+               local sources = vim.tbl_map(function(source)
+                  return { name = source.name }
+               end, cmp.get_config().sources)
+
+               local snippets_id = get_specified_source_id(sources, "snippets")
+
+               table.remove(sources, snippets_id)
+
+               cmp.setup.buffer({ sources = sources })
+            end,
          })
       end,
    },
