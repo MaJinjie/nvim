12ea1e68bb46f60fda7b1a7f7d6fcf70af45a68c
majinjie <2734883936@qq.com>
2024-11-23 23:56:40 +0800

    fix(keymaps): 添加对dotfiles的lazygit打开快捷键

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -9,3 +9,8 @@ map({ "i", "x", "n", "s" }, "<C-s>", function()
    LazyVim.format.format({ force = true })
    vim.cmd("w")
 end, { desc = "Save File And Format File" })
+
+-- lazygit
+map("n", "<leader>g.", function()
+   LazyVim.lazygit({ args = { "--git-dir", vim.env.DOT_GIT_DIR, "--work-tree", vim.env.DOT_GIT_WORK_TREE } })
+end, {desc = "Lazygit (Dotfiles)"})
