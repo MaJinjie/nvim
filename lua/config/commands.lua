868c091cf95dd8e5cd5ef67e956b6524dcd41a4e
majinjie <2734883936@qq.com>
2025-01-25 00:22:10 +0800

    fix: tmux navigate(add follow_zoomed)

--- lua/config/commands.lua
+++ lua/config/commands.lua
@@ -14,25 +14,33 @@ do
   end
 
   ---@param dir "h"|"j"|"k"|"l"|"p"
-  local function navigate(dir)
+  ---@param opts? {follow_zoomed?:boolean}
+  local function navigate(dir, opts)
+    opts = opts or {}
+    opts.follow_zoomed = vim.F.if_nil(opts.follow_zoomed, false)
+    local is_tmux = vim.env.TMUX
     local winnr = vim.api.nvim_get_current_win()
-    if not (dir == "p" and tmux_is_last_pane and (vim.env.TMUX and tmux_status("#{window_panes}") - 1 > 0)) then
+    if not (dir == "p" and tmux_is_last_pane and (is_tmux and tmux_status("#{window_panes}") - 1 > 0)) then
       vim.cmd("wincmd " .. dir)
     end
-    if vim.env.TMUX and winnr == vim.api.nvim_get_current_win() then
+    if is_tmux and winnr == vim.api.nvim_get_current_win() then
       if dir == "p" then
         vim.fn.system("tmux selectp -l")
       else
-        local directory = ({ h = "left", j = "bottom", k = "top", l = "right" })[dir]
+        local direction = ({ h = "left", j = "bottom", k = "top", l = "right" })[dir]
         local flag = ({ h = "D", j = "D", k = "U", l = "R" })[dir]
-        vim.fn.system(("tmux if -F '#{pane_at_%s}' '' 'selectp -Z%s'"):format(directory, flag))
+        if opts.follow_zoomed then
+          vim.fn.system(("tmux selectp -Z%s"):format(flag))
+        else
+          vim.fn.system(("tmux if -F '#{pane_at_%s}' '' 'selectp -Z%s'"):format(direction, flag))
+        end
       end
       tmux_is_last_pane = true
     end
   end
 
   command("TmuxNavigate", function(args)
-    navigate(args.args)
+    navigate(args.args, { follow_zoomed = true })
   end, {
     desc = "Tmux Navigate",
     nargs = 1,
